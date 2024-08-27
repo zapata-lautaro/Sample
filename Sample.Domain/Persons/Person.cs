40a73fae65989380b45861d65c4900110dcc9047
Lautaro Zapata <lautaro.zapata08@gmail.com>
2024-08-26 22:58:55 -0300

    Add project files.

---
+++ Sample.Domain/Persons/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace Sample.Domain.Persons
+{
+    public sealed class Person
+    {
+        private Person(
+        string id,
+        string name,
+        string language,
+        string bio,
+        decimal version)
+        {
+            Id = id;
+            Name = name;
+            Language = language;
+            Bio = bio;
+            Version = version;
+        }
+
+        public string Id { get; private set; }
+        public string Name { get; private set; }
+        public string Language { get; private set; }
+        public string Bio { get; private set; }
+        public decimal Version { get; private set; }
+    }
+}
